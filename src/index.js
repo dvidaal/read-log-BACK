7d8e8399841002c23a91417e5a7859f08e5be17e
Diana Vidal <dvidaal@gmail.com>
2024-02-20 20:11:26 +0100

    Delete function in index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
+import "./loadEnvironment.js";
 import connectDataBase from "./database/connectDatabase.js";
 import startServer from "./server/startServer.js";
-import "./loadEnvironment.js";
 
 const mongoUrl = process.env.MONGODB_CONNECTION_URL;
 const port = process.env.PORT ?? 4000;
 
-(async () => {
-  await connectDataBase(mongoUrl);
-  await startServer(+port);
-})();
+await connectDataBase(mongoUrl);
+await startServer(+port);
