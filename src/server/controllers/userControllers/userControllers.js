7010919f461484ad0392f116c5b25fb51d2664ca
Diana Vidal <dvidaal@gmail.com>
2024-02-22 18:44:01 +0100

    Fix login we are not gonna use bcrypt

--- src/server/controllers/userControllers/userControllers.js
+++ src/server/controllers/userControllers/userControllers.js
@@ -8,12 +8,21 @@ export const loginUser = async (req, res) => {
     const user = await User.findOne({ username });
     if (!user) {
       console.log("User not found");
+      return res.status(404).json({ message: "User not found" });
     }
 
-    if (!(await bcrypt.compare(password, user.password))) {
+    if (password === user.password) {
+      console.log("De locos pa'lante");
+    } else {
       console.log("Wrong password");
+      return res.status(401).json({ message: "Wrong password" });
     }
+
+    console.log("Login successful");
+
+    res.status(200).json({ message: "Login successful" });
   } catch (error) {
     console.log(error, "ka pasao");
+    res.status(500).json({ message: "Internal server error" });
   }
 };
