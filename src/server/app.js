25f777a81f339b7ec39822611c988a205f7eacd3
Diana Vidal <dvidaal@gmail.com>
2024-04-02 21:38:04 +0200

    Create solution for CORS error

--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import { usersRouters } from "./routers/usersRouters/usersRouters.js";
 import { booksRouters } from "./routers/booksRouters/booksRouters.js";
@@ -5,6 +6,18 @@ import { booksRouters } from "./routers/booksRouters/booksRouters.js";
 export const app = express();
 app.disable("x-powered-by");
 
+const corsOptions = {
+  credentials: true,
+  origin: [
+    process.env.CORS_ALLOWED_ORIGIN_LOCAL,
+    process.env.CORS_ALLOWED_ORIGIN_PRODUCTION,
+    process.env.CORS_ALLOWED_ORIGIN_PRODUCTION_1,
+    process.env.CORS_ALLOWED_ORIGIN_PRODUCTION_2,
+  ],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use("/read-log", booksRouters);
+app.use("/read-log", cors(corsOptions), booksRouters);
 app.use("/users", usersRouters);
