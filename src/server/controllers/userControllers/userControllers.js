294077cb5f1aa6d55f9d6251c7003c88c47d88af
Diana Vidal <dvidaal@gmail.com>
2024-05-14 15:30:22 +0200

    Create logic for jsonwebtoken

--- src/server/controllers/userControllers/userControllers.js
+++ src/server/controllers/userControllers/userControllers.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import User from "../../../database/models/Users/User.js";
 
 export const loginUser = async (req, res) => {
@@ -12,14 +13,20 @@ export const loginUser = async (req, res) => {
 
     if (password === user.password) {
       console.log("De locos pa'lante");
+
+      const token = jwt.sign(
+        { userId: user._id, username: user.username },
+        "testingHowItWOrks123!",
+        { expiresIn: "1h" }
+      );
+
+      console.log("TOKENSITO ", token);
+
+      return res.status(200).json({ message: "Login succesful", token });
     } else {
       console.log("Wrong password");
       return res.status(401).json({ message: "Wrong password" });
     }
-
-    console.log("Login successful");
-
-    res.status(200).json({ message: "Login successful" });
   } catch (error) {
     console.log(error, "ka pasao");
     res.status(500).json({ message: "Internal server error" });
