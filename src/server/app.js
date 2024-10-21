f09ab436168773b532bbb7a783878c881280b304
Diana Vidal <dvidaal@gmail.com>
2024-10-21 13:47:49 +0200

    Add route to create a book

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,43 @@
+// import cors from "cors";
+// import morgan from "morgan";
+// import express from "express";
+// import { usersRouters } from "./routers/usersRouters/usersRouters.js";
+// import { booksRouters } from "./routers/booksRouters/booksRouters.js";
+
+// export const app = express();
+// app.disable("x-powered-by");
+// app.use(morgan("dev"));
+// app.use(express.json());
+
+// const corsOptions = {
+//   credentials: true,
+//   origin: [
+//     process.env.CORS_ALLOWED_ORIGIN_LOCAL,
+//     process.env.CORS_ALLOWED_ORIGIN_PRODUCTION,
+//     process.env.CORS_ALLOWED_ORIGIN_PRODUCTION_1,
+//     process.env.CORS_ALLOWED_ORIGIN_PRODUCTION_2,
+//   ],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.json());
+// app.get("/", (req, res) => {
+//   res.send("<h1>Front Page Test</h1>");
+// });
+// app.use("/read-log", cors(corsOptions), booksRouters);
+// app.use("/users", usersRouters);
+
 import cors from "cors";
+import morgan from "morgan";
 import express from "express";
 import { usersRouters } from "./routers/usersRouters/usersRouters.js";
 import { booksRouters } from "./routers/booksRouters/booksRouters.js";
 
 export const app = express();
 app.disable("x-powered-by");
+app.use(morgan("dev"));
+app.use(express.json());
 
 const corsOptions = {
   credentials: true,
@@ -18,6 +51,10 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use("/read-log", cors(corsOptions), booksRouters);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Front Page Test</h1>");
+});
+
+app.use("/read-log", booksRouters);
 app.use("/users", usersRouters);
--- src/server/routers/booksRouters/booksRouters.js
+++ src/server/routers/booksRouters/booksRouters.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getBooks } from "../../controllers/booksControllers/booksControllers.js";
+import {
+  createBook,
+  getBooks,
+} from "../../controllers/booksControllers/booksControllers.js";
 
 export const booksRouters = Router();
 
 booksRouters.get("/", getBooks);
+booksRouters.post("/", createBook);
 
 export default booksRouters;
